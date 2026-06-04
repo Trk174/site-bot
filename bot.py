c9fecb2850bcf38b7ea7d972a743e2d6f8a090e4
MrKey <166393241+Trk174@users.noreply.github.com>
2026-06-04 15:23:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import time
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.service import Service
+#import time
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+#options = Options()
+#options.add_argument("--headless")
+#options.add_argument("--no-sandbox")
+#options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome(options=options)
 
 # Token secret — trebuie să fie identic cu KEEPALIVE_TOKEN din index.php
-KEEPALIVE_TOKEN = "cy8r4_k33p_2024_x7z"
+#KEEPALIVE_TOKEN = "cy8r4_k33p_2024_x7z"
 
 # Accesăm homepage-ul cu token-ul secret, anti-botul ne lasă să trecem direct
-url = f"http://cyber4.rf.gd/premium.php/?keepalive={KEEPALIVE_TOKEN}"
+#url = f"http://cyber4.rf.gd/premium.php/?keepalive={KEEPALIVE_TOKEN}"
 
 try:
     driver.get(url)
